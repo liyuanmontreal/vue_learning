7735d1a2b48a35db425876cdd0cb17c8f1fc315a
Yuan Li <65098066+liyuanmontreal@users.noreply.github.com>
2024-05-07 09:16:29 -0400

    add teleport example

---
+++ Teleport/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <h1>Teleport</h1>
+  <p>Teleport is a built-in component that allows us to "teleport" a part of a component's template into a DOM node that exists outside the DOM hierarchy of that component.</p>
+  <p>With &lt;teleport to="body"&gt; we move the red &lt;div&gt; from inside the component to the body tag.</p>
+  <comp-one></comp-one>
+
+    <div id="modal1">div in parent page </div>
+
+
+  <div>   
+    <teleport to="#modal1">
+      <div class="yellowDiv">This modal is teleported to target from parent page.</div>
+    </teleport>
+
+    <teleport to="#div4">
+      <div id="redDiv">Hello! I will show in the div 4 </div>
+    </teleport>
+    <teleport to="#modal2">
+      <div class="yellowDiv">This modal is teleported to target  from parent page.</div>
+    </teleport>
+
+  </div>
+
+</template>
+
+<script></script>
+
+<style>
+  #app {
+    width: 350px;
+    margin: 10px;
+  }
+  #app > div {
+    border: solid black 2px;
+    padding: 10px;
+    margin-top: 10px;
+    background-color: rgb(186, 228, 255);
+  }
+  h2 {
+    text-decoration: underline;
+  }
+
+  #redDiv {
+    background-color: lightcoral;
+    margin: 10px;
+    padding: 10px;
+    display: inline-block;
+  }
+  .yellowDiv {
+    background-color: yellow;
+    margin: 10px;
+    padding: 10px;
+    display: inline-block;
+  }
+</style>     
---
+++ Teleport/CompOne.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+      <h2>Component without Teleport</h2>
+      <p>This is the inside of the component.</p>        
+        <div id="redDiv">Hello! I will stay in the component </div>          
+  </div>
+
+  <div>
+      <h2>Component with Teleport</h2>
+      <p>My content will show on the body.</p>
+      <Teleport to="body">
+        <div id="redDiv">Hello! I will show in the body </div>
+      </Teleport>
+  </div>
+
+
+  <div>
+    <h2>Component with Telepoort 3</h2>
+    <p>content from component 3 will  show in the div 4.</p>
+    <Teleport to="#div4">
+      <div id="blueDiv">Hello! I will show in the div 4 </div>
+    </Teleport> 
+</div>
+
+<div id="div4">
+  <h2>Component with Telepoort 4</h2>
+  <p>content will show here</p>
+</div>
+
+ 
+<div id="modal2">div in child component </div>
+
+
+    <teleport to="#modal1">
+      <div class="blueDiv">This modal is teleported to target from component.</div>
+    </teleport> 
+
+    <teleport to="#modal2">
+      <div class="blueDiv">This modal is teleported to target from component</div>
+    </teleport> 
+
+
+      
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  #redDiv {
+    background-color: lightcoral;
+    margin: 10px;
+    padding: 10px;
+    display: inline-block;
+  }
+  .yellowDiv {
+    background-color: yellow;
+    margin: 10px;
+    padding: 10px;
+    display: inline-block;
+  }
+  .blueDiv {
+    background-color: blue;
+    margin: 10px;
+    padding: 10px;
+    display: inline-block;
+  }
+</style>                  
